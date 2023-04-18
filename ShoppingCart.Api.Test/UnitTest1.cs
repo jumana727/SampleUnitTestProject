3c90aceacdcc09d5561f980af4c4421fbb4d9aa2
sharanphadke <sharan.phadke@matrixcomsec.com>
2023-04-18 10:56:06 +0530

    UPDATE: Add More unit tests

--- ShoppingCart.Api.Test/UnitTest1.cs
+++ ShoppingCart.Api.Test/UnitTest1.cs
@@ -19,38 +19,38 @@ namespace ProjectNamespace.Test
             _repository = A.Fake<IProductsReference>();
             controller = new ProductsController(_repository);
         }
-        // [TestMethod]
-        // public void MethodName()
-        // {
+        [TestMethod]
+        public void MethodName()
+        {
             
-        //     A.CallTo(() => _repository.GetById("12")).Returns( new Product() { Id = new string("12"),
-        //             Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M });
+            A.CallTo(() => _repository.GetById("12")).Returns( new Product() { Id = new string("12"),
+                    Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M });
             
-        //     IActionResult res = controller.Get("12");
+            IActionResult res = controller.Get("12");
             
-        //     var result = res as OkObjectResult;
+            var result = res as OkObjectResult;
  
-        //     var prod = result.Value as Product;
+            var prod = result.Value as Product;
 
-        //     prod.Id.Should().Be("12");
-        // }
+            prod.Id.Should().Be("12");
+        }
 
-        // [TestMethod]
-        // public void ProductsController_GetAllProducts_testApi()
-        // {
-        //     A.CallTo(() => _repository.GetAllItems()).Returns( new List<Product>(){
-        //          new Product() { Id = new string("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
-        //             Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M },
-        //         new Product() { Id = new string("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
-        //             Name = "Diary Milk", Manufacturer="Cow", Price = 4.00M },
-        //         new Product() { Id = new string("33704c4a-5b87-464c-bfb6-51971b4d18ad"),
-        //             Name = "Frozen Pizza", Manufacturer="Uncle Mickey", Price = 12.00M }
-        //     });
-        //     IActionResult result = controller.GetProducts();
-        //     var resultObj = result as OkObjectResult;
-        //     var products = resultObj.Value as List<Product>;
-        //     products.Count().Should().Be(2);
-        // }
+        [TestMethod]
+        public void ProductsController_GetAllProducts_testApi()
+        {
+            A.CallTo(() => _repository.GetAllItems()).Returns( new List<Product>(){
+                 new Product() { Id = new string("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                    Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M },
+                new Product() { Id = new string("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
+                    Name = "Diary Milk", Manufacturer="Cow", Price = 4.00M },
+                new Product() { Id = new string("33704c4a-5b87-464c-bfb6-51971b4d18ad"),
+                    Name = "Frozen Pizza", Manufacturer="Uncle Mickey", Price = 12.00M }
+            });
+            IActionResult result = controller.GetProducts();
+            var resultObj = result as OkObjectResult;
+            var products = resultObj.Value as List<Product>;
+            products.Count().Should().Be(2);
+        }
 
         [TestMethod]
         public void TestProductService()
@@ -59,7 +59,7 @@ namespace ProjectNamespace.Test
             IEnumerable<Product> products = product.GetAllItems();
             List<Product> cart = ProductsService._shoppingCart;
             int count = cart.Count();
-            count.Should().Be(2);
+            count.Should().Be(3);
             product.Should().NotBeNull();
         }
 
